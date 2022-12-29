b9c0546d12493f476cae34708e8b45bcba87563c
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-12-29 02:34:45 -0500

    debug statement for emotion classifier

--- emotions.py
+++ emotions.py
@@ -4,11 +4,14 @@ tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-emotion")
 model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-emotion")
 
 def get_emotion(text):
-  input_ids = tokenizer.encode(text + '</s>', return_tensors='pt')
+  try:
+    input_ids = tokenizer.encode(text + '</s>', return_tensors='pt')
 
-  output = model.generate(input_ids=input_ids,
-               max_length=2)
-  
-  dec = [tokenizer.decode(ids) for ids in output]
-  label = dec[0]
-  return label[6:len(label)]
+    output = model.generate(input_ids=input_ids,
+                max_length=2)
+    
+    dec = [tokenizer.decode(ids) for ids in output]
+    label = dec[0]
+    return label[6:len(label)]
+  except:
+    print("Error")
