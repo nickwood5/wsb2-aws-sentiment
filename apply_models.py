e700dfbcb8a9d1460cc38d9a5ffa3901aa61d9c8
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-12-29 01:57:10 -0500

    Add try/catch block to catch errors

--- apply_models.py
+++ apply_models.py
@@ -24,33 +24,37 @@ def calculate_sentiment(index, file_name):
     loop_start = time()
 
     for i, c in enumerate(data["content"]):
-        compound, overall_sentiment = sentiment(c)
-        print("{}: {} of {}, compound = {}".format(index, i+1, len(data), compound), flush=True)
-        print("NEW {}: {} of {}, compound = {}".format(index, i+1, len(data), compound), flush=True)
+        try:
+            compound, overall_sentiment = sentiment(c)
+            print("{}: {} of {}, compound = {}".format(index, i+1, len(data), compound), flush=True)
+            print("NEW {}: {} of {}, compound = {}".format(index, i+1, len(data), compound), flush=True)
 
-        all_compounds.append(compound)
-        all_overall_sentiments.append(overall_sentiment)
+            all_compounds.append(compound)
+            all_overall_sentiments.append(overall_sentiment)
 
-        emotion = get_emotion(c)
-        all_emotions.append(emotion)
+            emotion = get_emotion(c)
+            all_emotions.append(emotion)
 
-        loop += 1
+            loop += 1
 
-        if loop >= loop_range:
-            loop_end = time()
+            if loop >= loop_range:
+                loop_end = time()
 
-            elapsed_time = loop_end - loop_start
+                elapsed_time = loop_end - loop_start
 
-            average = elapsed_time / loop_range
-            remaining = len(data) - (i + 1)
-            estimated_time = remaining * average
-            
-            print("{}: {} of {}, est. time remaining: {}s".format(index, i+1, len(data), round(estimated_time, 0)), flush=True)
-            loop = 0
-            
+                average = elapsed_time / loop_range
+                remaining = len(data) - (i + 1)
+                estimated_time = remaining * average
+                
+                print("{}: {} of {}, est. time remaining: {}s".format(index, i+1, len(data), round(estimated_time, 0)), flush=True)
+                loop = 0
+                
 
-            loop_start = loop_end
-        sys.stdout.flush()
+                loop_start = loop_end
+            sys.stdout.flush()
+        except:
+            print("Some error occured", flush=True)
+            sys.stdout.flush()
 
     data["sentiment_score"] = all_compounds
     data["sentiment_label"] = all_overall_sentiments
